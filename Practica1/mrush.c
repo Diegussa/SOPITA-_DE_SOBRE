37954fe28adc89e9462fa67b7e9c6495e1c8c93b
Hernanzueloo <alejandrogarciahernando1@gmail.com>
2023-02-13 18:15:15 +0100

    Version final 1

--- Practica1/mrush.c
+++ Practica1/mrush.c
@@ -7,16 +7,66 @@
 #include "pow.h"
 
 #define MAX POW_LIMIT
+#define MAX_HILOS 10
 
 typedef struct
 {
     long ep, eu, res;
 } entradaHash;
 
-void *func_minero(void *arg) {
+void *func_minero(void *arg);
+
+long minero(int nHilos, long busq);
+
+int main(int argc, char *argv[])
+{
+    int rc[MAX_HILOS], t1, t2, i;
+    long solucion, busq;
+
+    /*Control de errores*/
+    t1 = clock();
+    if ((atoi(argv[1]) < 0) || (atoi(argv[1]) > POW_LIMIT) || (atoi(argv[2]) < 0) || (atoi(argv[3]) < 0) || (atoi(argv[3]) > MAX_HILOS))
+    {
+        printf("\n\nError en los parametros de entrada");
+        return 1;
+    }
+
+    /*Buscamos los elementos*/
+    busq = atol(argv[1]);
+    for (i = 0; i < atoi(argv[2]); i++)
+    {
+        solucion = minero(atoi(argv[3]), busq);
+
+        /*Impresion de la solucion*/
+        if (pow_hash(solucion) == busq)
+        {
+            printf("\nSolution accepted: %08ld --> %08ld", busq, solucion);
+        }
+        else
+        {
+            printf("\nSolution rejected: %08ld !-> %08ld", busq, solucion);
+            printf("\nThe soluction has been invalidated");
+            printf("\nMonitor exited with status 0");
+            printf("\nMiner exited with status 0");
+            return 1;
+        }
+        busq=solucion;
+    }
+
+
+    t2 = clock();
+    printf("\nTime spent %d\n", t2 - t1);
+    printf("\nMonitor exited with status 0");
+    printf("\nMiner exited with status 0");
+
+    return 0;
+}
+
+void *func_minero(void *arg)
+{
     int i;
-    long x=-1;
-    entradaHash *e=(entradaHash*)arg;
+    long x = -1;
+    entradaHash *e = (entradaHash *)arg;
 
     for (i = e->ep; (i < e->eu) && (x <= 0); i++)
     {
@@ -26,53 +76,45 @@ void *func_minero(void *arg) {
         }
     }
 
-   pthread_exit((void *)x);
+    pthread_exit((void *)x);
 }
 
-int main(int argc, char *argv[]) {
-   pthread_t thread1, thread2;
-   int rc, rc2, t1, t2;
-   entradaHash t;
-   void *sol, *sol2;
+long minero(int nHilos, long busq)
+{
+    int i, rc[MAX_HILOS];
+    entradaHash t[MAX_HILOS];
+    pthread_t threads[MAX_HILOS];
+    void *sol[MAX_HILOS];
 
-    t1=clock();
-/*Creacion del hilo 1*/
-    t.ep = 0;
-    t.eu = MAX/2;
-    t.res = (long)pow_hash(MAX/2+1000);
+    /*Creacion de los hilos*/
+    for (i = 0; i < nHilos; i++)
+    {
+        t[i].ep = ((long)MAX / nHilos) * i;
+        t[i].eu = ((long)MAX / nHilos) * (i + 1);
+        t[i].res = busq;
+        rc[i] = pthread_create(&threads[i], NULL, func_minero, (void *)&t[i]);
+        if (rc[i])
+        {
+            printf("Error creando el hilo %d", i);
+            return 1;
+        }
+    }
 
-   rc = pthread_create(&thread1, NULL, func_minero, (void *)&t);
-   if (rc) {
-      printf("Error creating thread 1\n");
-      exit(-1);
-   }
-/*Creacion del hilo 2*/
-    t.ep = MAX/2+1;
-    t.eu = MAX;
-    t.res = (long)pow_hash(MAX/2+1000);
+    /*Joins de los hilos*/
+    for (i = 0; i < nHilos; i++)
+    {
+        rc[i] = pthread_join(threads[i], &sol[i]);
+        if (rc[i])
+        {
+            printf("Error joining thread %d\n", i);
+            return -1;
+        }
+    }
 
-   rc = pthread_create(&thread2, NULL, func_minero, (void *)&t);
-   if (rc) {
-      printf("Error creating thread 1\n");
-      exit(-1);
-   }
-
-
-   rc = pthread_join(thread1, &sol);
-   if (rc) {
-      printf("Error joining thread 1\n");
-      exit(-1);
-   }
-
-   rc2 = pthread_join(thread2, &sol2);
-   if (rc2) {
-      printf("Error joining thread 2\n");
-      exit(-1);
-   }
-   t2=clock();
-
-   printf("Thread 1 returned value: %ld\n   Time spent: %d", (long)sol, t2-t1);
-   printf("\nThread 2 returned value: %ld\n   Time spent: %d", (long)sol2, t2-t1);
-
-   return 0;
+    for (i = 0; i < nHilos; i++)
+    {
+        if((long)sol[i]!=-1){
+            return (long)sol[i];
+        }
+    }
 }
