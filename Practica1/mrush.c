895873e9408e62c227dc825f7b6314ffd9d74790
Hernanzueloo <alejandrogarciahernando1@gmail.com>
2023-02-13 15:37:50 +0100

    actu2

--- Practica1/mrush.c
+++ Practica1/mrush.c
@@ -1,14 +1,56 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include "pow.h"
 
+#define MAX 100000000
 
-int main(){
-    printf("El valor de f(%d) es %ld", 1, pow_hash(1));
+typedef struct
+{
+    long ep, eu, res;
+} entradaHash;
 
+void *print_message_function(void *threadarg);
 
-    return 0;
+int main(int argc, char *argv[]) {
+   pthread_t thread1;
+   int rc;
+   entradaHash t;
+   void *status;
+
+/*Creacion del hilo*/
+    t.ep = 0;
+    t.eu = MAX;
+    t.res = atoi(argv[1]);
+
+   rc = pthread_create(&thread1, NULL, print_message_function, (void *)&t);
+   if (rc) {
+      printf("Error creating thread 1\n");
+      exit(-1);
+   }
+
+   rc = pthread_join(thread1, &status);
+   if (rc) {
+      printf("Error joining thread 1\n");
+      exit(-1);
+   }
+
+   printf("Thread 1 returned value: %ld\n", (long)status);
+
+   return 0;
+}
+
+void *print_message_function(void *arg) {
+    int i;
+    long x=-1;
+    entradaHash *e=(entradaHash*)arg;
+
+    for (i = e->ep; (i < e->eu) && (x <= 0); i++)
+    {
+        if ((long)pow_hash(i) == e->res)
+        {
+            x = i;
+        }
+    }
+
+   pthread_exit((void *)x);
 }
