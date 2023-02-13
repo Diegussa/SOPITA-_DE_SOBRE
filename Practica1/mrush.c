572f971577b4abe0005192452340e8ac828d22fe
Hernanzueloo <alejandrogarciahernando1@gmail.com>
2023-02-13 17:06:37 +0100

    actu 3

--- Practica1/mrush.c
+++ Practica1/mrush.c
@@ -1,45 +1,19 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include "pow.h"
 
-#define MAX 100000000
+#define MAX POW_LIMIT
 
 typedef struct
 {
     long ep, eu, res;
 } entradaHash;
 
-void *print_message_function(void *threadarg);
-
-int main(int argc, char *argv[]) {
-   pthread_t thread1;
-   int rc;
-   entradaHash t;
-   void *status;
-
-/*Creacion del hilo*/
-    t.ep = 0;
-    t.eu = MAX;
-    t.res = atoi(argv[1]);
-
-   rc = pthread_create(&thread1, NULL, print_message_function, (void *)&t);
-   if (rc) {
-      printf("Error creating thread 1\n");
-      exit(-1);
-   }
-
-   rc = pthread_join(thread1, &status);
-   if (rc) {
-      printf("Error joining thread 1\n");
-      exit(-1);
-   }
-
-   printf("Thread 1 returned value: %ld\n", (long)status);
-
-   return 0;
-}
-
-void *print_message_function(void *arg) {
+void *func_minero(void *arg) {
     int i;
     long x=-1;
     entradaHash *e=(entradaHash*)arg;
@@ -53,4 +27,52 @@ void *print_message_function(void *arg) {
     }
 
    pthread_exit((void *)x);
+}
+
+int main(int argc, char *argv[]) {
+   pthread_t thread1, thread2;
+   int rc, rc2, t1, t2;
+   entradaHash t;
+   void *sol, *sol2;
+
+    t1=clock();
+/*Creacion del hilo 1*/
+    t.ep = 0;
+    t.eu = MAX/2;
+    t.res = (long)pow_hash(MAX/2+1000);
+
+   rc = pthread_create(&thread1, NULL, func_minero, (void *)&t);
+   if (rc) {
+      printf("Error creating thread 1\n");
+      exit(-1);
+   }
+/*Creacion del hilo 2*/
+    t.ep = MAX/2+1;
+    t.eu = MAX;
+    t.res = (long)pow_hash(MAX/2+1000);
+
+   rc = pthread_create(&thread2, NULL, func_minero, (void *)&t);
+   if (rc) {
+      printf("Error creating thread 1\n");
+      exit(-1);
+   }
+
+
+   rc = pthread_join(thread1, &sol);
+   if (rc) {
+      printf("Error joining thread 1\n");
+      exit(-1);
+   }
+
+   rc2 = pthread_join(thread2, &sol2);
+   if (rc2) {
+      printf("Error joining thread 2\n");
+      exit(-1);
+   }
+   t2=clock();
+
+   printf("Thread 1 returned value: %ld\n   Time spent: %d", (long)sol, t2-t1);
+   printf("\nThread 2 returned value: %ld\n   Time spent: %d", (long)sol2, t2-t1);
+
+   return 0;
 }
