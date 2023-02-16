debb6f9d1538258c3785ecb22fab09d8d0e85d49
Hernanzueloo <alejandrogarciahernando1@gmail.com>
2023-02-16 17:13:02 +0100

    ult 16 del 2

--- Practica1/mrush.c
+++ Practica1/mrush.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <time.h>
 #include <pthread.h>
-#include <assert.h>
 #include <sys/wait.h>
 #include "pow.h"
 
