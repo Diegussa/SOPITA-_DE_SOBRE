fa8daa68e5c3f66619758a2d534f901d2a2ec24a
Hernanzueloo <alejandrogarciahernando1@gmail.com>
2023-02-16 11:28:26 +0100

    actu 2 16 del 2

--- Practica1/mrush.c
+++ Practica1/mrush.c
@@ -5,6 +5,7 @@
 #include <time.h>
 #include <pthread.h>
 #include <assert.h>
+#include <sys/wait.h>
 #include "pow.h"
 
 #define MAX POW_LIMIT - 1
@@ -17,12 +18,11 @@ typedef struct
 
 void *func_minero(void *arg);
 
-long minero(int nHilos, long busq, int rondas);
+int minero(int nHilos, long busq, int rondas);
 
 int main(int argc, char *argv[])
 {
-    int rc[MAX_HILOS], t1, t2, i;
-    long solucion, busq;
+    int rc[MAX_HILOS], t1, t2, i, status;
 
     /*Control de errores*/
     t1 = clock();
@@ -33,31 +33,18 @@ int main(int argc, char *argv[])
     }
 
     /*Buscamos los elementos*/
-    busq = atol(argv[1]);
-    for (i = 0; i < atoi(argv[2]); i++)
+    status = minero(atoi(argv[3]), atol(argv[1]), (atoi(argv[2])));
+    if (status == EXIT_SUCCESS)
     {
-        solucion = minero(atoi(argv[3]), busq, (atoi(argv[2])));
-
-        /*Impresion de la solucion*/
-        if (pow_hash(solucion) == busq)
-        {
-            printf("\nSolution accepted: %08ld --> %08ld", busq, solucion);
-        }
-        else
-        {
-            printf("\nSolution rejected: %08ld !-> %08ld", busq, solucion);
-            printf("\nThe soluction has been invalidated");
-            printf("\nMonitor exited with status 0");
-            printf("\nMiner exited with status 0");
-            return 1;
-        }
-        busq = solucion;
+        printf("\nMiner exited with status 0");
+    }
+    else
+    {
+        printf("\nMiner exited with status 1");
     }
 
     t2 = clock();
     printf("\nTime spent %d\n", t2 - t1);
-    printf("\nMonitor exited with status 0");
-    printf("\nMiner exited with status 0");
 
     return 0;
 }
@@ -79,11 +66,11 @@ void *func_minero(void *arg)
     pthread_exit((void *)x);
 }
 
-long minero(int nHilos, long busq, int rondas)
+int minero(int nHilos, long busq, int rondas)
 {
-    int i, rc[MAX_HILOS], status, pipe1[2], pipe2[2], nbytes;
+    int i, j, rc[MAX_HILOS], pipe1[2], pipe2[2], nbytes, exitV;
     entradaHash t[MAX_HILOS];
-    long parPH[2], comp[2];
+    long parPH[2], comp[2] = {0, 0};
     pthread_t threads[MAX_HILOS];
     void *sol[MAX_HILOS];
     pid_t childpid;
@@ -100,9 +87,10 @@ long minero(int nHilos, long busq, int rondas)
         exit(EXIT_FAILURE);
     }
 
-    childpid = fork();/*0 si es el hijo y pid del hijo en el caso del padre*/
-    
-    if(childpid==-1){/*Control de errores*/
+    childpid = fork(); /*0 si es el hijo y pid del hijo en el caso del padre*/
+
+    if (childpid == -1)
+    { /*Control de errores*/
         perror("fork");
         exit(EXIT_FAILURE);
     }
@@ -110,85 +98,105 @@ long minero(int nHilos, long busq, int rondas)
     {
         close(pipe1[1]);
         close(pipe2[0]);
-        /*sleep(1);*/
-        
-        do{
-            nbytes=read(pipe1[0], parPH, sizeof(long)*2);
-            if(nbytes==-1){
-                printf("Error");
-                exit(EXIT_FAILURE);
-            }
-        } while (nbytes==0); 
 
-        /*Impresion de la solucion*/
-        if (pow_hash(parPH[1]) == parPH[0])
+        for (j = 0; j < rondas; j++)
         {
-            printf("\nSolution accepted: %08ld --> %08ld", parPH[0], parPH[1]);
-            nbytes = write(pipe2[1],parPH, sizeof(long)*2);
+            do
+            {
+                nbytes = read(pipe1[0], parPH, sizeof(long) * 2);
+                if (nbytes == -1)
+                {
+                    printf("Error");
+                    exit(EXIT_FAILURE);
+                }
+            } while (nbytes == 0);
+
+            /*Impresion de la solucion*/
+            if (pow_hash(parPH[1]) == parPH[0])
+            {
+                printf("\nSolution accepted: %08ld --> %08ld", parPH[0], parPH[1]);
+                nbytes = write(pipe2[1], parPH, sizeof(long) * 2);
+            }
+            else
+            {
+                printf("\nSolution rejected: %08ld !-> %08ld", parPH[0], parPH[1]);
+                printf("\nThe solution has been invalidated");
+                parPH[0] = -1;
+                parPH[1] = -1;
+                nbytes = write(pipe2[1], parPH, sizeof(long) * 2);
+                exit(EXIT_FAILURE);
+                return -1;
+            }
         }
-        else
-        {
-            printf("\nSolution rejected: %08ld !-> %08ld", parPH[0], parPH[1]);
-            printf("\nThe soluction has been invalidated");
-            parPH[0]=-1;
-            parPH[1]=-1;
-            nbytes = write(pipe2[1],parPH, sizeof(long)*2);
-            exit(EXIT_FAILURE);
-            return -1;
-        }
+
+        exit(EXIT_SUCCESS);
     }
-    else{
+    else
+    {
         close(pipe1[0]);
         close(pipe2[1]);
 
-        /*Creacion de los hilos*/
-    for (i = 0; i < nHilos; i++)
-    {
-        t[i].ep = ((long)MAX / nHilos) * i;
-        t[i].eu = ((long)MAX / nHilos) * (i + 1);
-        t[i].res = busq;
-        rc[i] = pthread_create(&threads[i], NULL, func_minero, (void *)&t[i]);
-        if (rc[i])
+        for (j = 0; j < rondas; j++)
         {
-            printf("Error creando el hilo %d", i);
-            return 1;
+            /*Creacion de los hilos*/
+            for (i = 0; i < nHilos; i++)
+            {
+                t[i].ep = ((long)MAX / nHilos) * i;
+                t[i].eu = ((long)MAX / nHilos) * (i + 1);
+                t[i].res = busq;
+                rc[i] = pthread_create(&threads[i], NULL, func_minero, (void *)&t[i]);
+                if (rc[i])
+                {
+                    printf("Error creando el hilo %d", i);
+                    return 1;
+                }
+            }
+
+            /*Joins de los hilos*/
+            for (i = 0; i < nHilos; i++)
+            {
+                rc[i] = pthread_join(threads[i], &sol[i]);
+                if (rc[i])
+                {
+                    printf("Error joining thread %d\n", i);
+                    return -1;
+                }
+            }
+
+            /*Almacenar la solucion de los hilos*/
+            for (i = 0; i < nHilos; i++)
+            {
+                if ((long)sol[i] != -1)
+                {
+                    parPH[0] = (long)busq;
+                    parPH[1] = (long)sol[i];
+                    nbytes = write(pipe1[1], parPH, sizeof(long) * 2);
+                    i = nHilos; /*Para salir del bucle*/
+                }
+            }
+
+            do
+            {
+                nbytes = read(pipe2[0], comp, sizeof(long) * 2);
+                if ((nbytes == -1) || comp[0] == -1)
+                {
+                    printf("Error leyendo pipe2");
+                    exit(EXIT_FAILURE);
+                    return -1;
+                }
+            } while (comp[0] != parPH[0]);
+            busq = parPH[1];
         }
     }
 
-        /*Joins de los hilos*/
-        for (i = 0; i < nHilos; i++)
-        {
-            rc[i] = pthread_join(threads[i], &sol[i]);
-            if (rc[i])
-            {
-                printf("Error joining thread %d\n", i);
-                return -1;
-            }
-        }
-
-        for (i = 0; i < nHilos; i++)
-        {
-            if ((long)sol[i] != -1)
-            {
-                parPH[0]=(long)busq;
-                parPH[1]=(long)sol[i];
-                nbytes = write(pipe1[1],parPH, sizeof(long)*2);
-                i=nHilos;/*Para salir del bucle*/
-            }
-        }
-
-        do{
-            nbytes=read(pipe2[0], comp, sizeof(long)*2);
-            if((nbytes==-1)||comp[0]==-1){
-                printf("Error leyendo pipe2");
-                exit(EXIT_FAILURE);
-                return -1;
-            }
-        }while(comp[0]!=parPH[0]);
-
-        printf("Recibido los comps del pip2 %ld %ld", comp[0], comp[1]);
-    }
+    wait(&exitV); // espera a que el proceso hijo termine
     
-    exit(EXIT_SUCCESS);
-    return 0;
+    if (exitV == EXIT_FAILURE)
+    {
+        printf("\nMonitor exited with status 1");
+        return EXIT_FAILURE;
     }
+
+    printf("\nMonitor exited with status 0");
+    return EXIT_SUCCESS;
+}
