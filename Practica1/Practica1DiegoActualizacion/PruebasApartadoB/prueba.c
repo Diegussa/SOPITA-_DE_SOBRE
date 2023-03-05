67f5db8314e6ff7a8ef195b2a5fadb0c3f3e5fff
Diegussa <diegorortiz03@gmail.com>
2023-03-05 10:38:47 +0100

    Cambios

--- Practica1/Practica1DiegoActualizacion/PruebasApartadoB/prueba.c
+++ Practica1/Practica1DiegoActualizacion/PruebasApartadoB/prueba.c
@@ -8,141 +8,68 @@
 
 #define POW_LIMIT 99997669
 #define MAX POW_LIMIT - 1
-#define CONT 23
+int a;
 
-int padre ( int Escritura, int Lectura){
-    int k = 0,m, nbytes, status;
-    while ( 1){
-        nbytes = write ( Escritura, &k,sizeof(int));
-     if (nbytes == -1)
+void *func_minero(void *arg)
+{
+    int i,*m=(int*)arg;
+    long x =0;
+
+    for (i = 0; (a == 0); i++)
+    {
+        
+
+        if ((long)i == (long)*m)
         {
-            perror("read");
-            close(Escritura);
-            wait(&status);
-            printf("Son exited with status %d\n",status);
-
-            close(Lectura);
-            exit(EXIT_FAILURE);
-        }
-        if (nbytes==0){
-            close(Escritura);
-            wait(&status);
-            printf("Son exited with status %d\n",status);
-            close(Lectura);
-            exit(EXIT_FAILURE);
-        }
-        nbytes = read(Lectura, &k, sizeof(int)*1);
-        if (nbytes == -1)
-        {
-            perror("read");
-            close(Escritura);
-            wait(&status);
-            printf("Son exited with status %d\n",status);
-            close(Lectura);
-            exit(EXIT_FAILURE);
-        }
-        if (nbytes==0){
-
-            perror("read");
-            close(Escritura);
-            wait(&status);
-            printf("49: %d Son exited with status %d\n",m,status);
-            
-            close(Lectura);
-            exit(EXIT_FAILURE);
-        }
-         k+=1;
-
-        printf ( "PADRE :  %d \n", k);
-
-        if ( k==2*CONT){
-            close(Escritura);
-            wait(&status);
-            printf(" Son exited with status %d\n",status);
-            close(Lectura);
-            return k;
+            a=1;
+            x=-1;
+            pthread_exit((void *)x);
+            return ;
         }
     }
-}
-
-int hijo (int Escritura, int Lectura){
-    int k = 1,m, nbytes;
-
-    while (1){
-
-    
-    nbytes = read(Lectura, &k, sizeof(int)*1);
-        if (nbytes == -1)
-        {
-            perror("read");
-            close(Escritura);
-            close(Lectura);
-            if( m == 30) exit(EXIT_SUCCESS);
-
-            exit(EXIT_FAILURE);
-        }
-        if (nbytes==0){
-            perror("readv");
-            close(Escritura);            
-            close(Lectura);
-            if ( k == 2*CONT -1){
-
-                 exit(EXIT_SUCCESS);
-            }
-            exit(EXIT_FAILURE);
-        }
-    k+=1;
-    printf ("HIJO %d \n", k);
-    nbytes = write ( Escritura, &k,sizeof(int));
-     if (nbytes == -1)
-        {
-            perror("read");
-            close(Escritura);
-            close(Lectura);
-
-            exit(EXIT_FAILURE);
-        }
-        if (nbytes==0){
-            close(Escritura);
-            close(Lectura);
-
-            exit(EXIT_FAILURE);
-        }
-    }
+    x=i;
+    pthread_exit((void *)x);
 }
 
 int main(){
-    int i,K,t1[100],busq=17,status,Pipe1[2],Pipe2[2];
+    int i,K,t1[100],busq=17;
+    pthread_t threads[100];
     int *sol[100];
-    int a=0;
-    pid_t pid;
-  
-
-    status = pipe(Pipe1);
-    if (status == -1)
-    {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-    status = pipe(Pipe2);
-    if (status == -1)
-    {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
+    a=0;
    
-    pid = fork();
-
-    if ( pid){
-        close(Pipe1[0]);
-        close(Pipe2[1]);
-       a = padre (Pipe1[1], Pipe2[0]);
-    }else{
-         close(Pipe1[1]);
-        close(Pipe2[0]);
-        hijo (Pipe2[1], Pipe1[0]);
+     /*Creacion de los hilos*/
+    for (i = 0; i < 100; i++)
+    {
+         t1[i]=INT_MAX;
+         if( i == 73)   
+            t1[i]= busq;
+         
+        K= pthread_create(&threads[i], NULL, func_minero, (void *)&t1[i]);
+        if (K)
+        {
+            printf("Error creando el hilo %d", i);
+            return 1;
+        }
     }
 
-    printf ( " a = %d, %d\n" , a, a==2*CONT);
+    /*Joins de los hilos*/
+    for (i = 0; i < 100; i++)
+    {
+        K= pthread_join(threads[i], &sol[i]);
+        if (K)
+        {
+            printf("Error joining thread %d\n", i);
+            return -1;
+        }
+    }
+
+    for (i = 0; i < 100; i++)
+    {
+       
+        
+            printf("%d %d\n",i, (long)sol[i]);
+
+        
+    }
      return EXIT_SUCCESS;
 }
