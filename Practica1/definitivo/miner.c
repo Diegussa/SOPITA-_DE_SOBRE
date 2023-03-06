f9fc1f58554910d54c9467ec0aa6717dafdf0939
Diegussa <diegorortiz03@gmail.com>
2023-03-06 18:38:56 +0100

    yrksfx

--- Practica1/definitivo/miner.c
+++ Practica1/definitivo/miner.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#include "pow.h"
 #include "miner.h"
 
 int encontrado;
--- Practica1/definitivo/miner.h
+++ Practica1/definitivo/miner.h
@@ -6,8 +6,11 @@
  *
  */
 
+#include "pow.h"
+
 #ifndef MINER_H
 #define MINER_H
+
 
 #define MAX POW_LIMIT-1
 #define MAX_HILOS 100
@@ -15,7 +18,7 @@
 typedef struct _entradaHash entradaHash;
 
 /**
- * @brief Prints the answer
+ * @brief Crea el proceso monitor, gestión de tuberias y llama a la función a minar
  *
  * @param nHilos numero de hilos a crear
  * @param nbusquedas numero de rondas que se haran
