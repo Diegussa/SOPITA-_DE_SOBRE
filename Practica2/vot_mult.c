bfabfbf0a83807fabd34a9a61586be0805ab0494
Hernanzueloo <alejandrogarciahernando1@gmail.com>
2023-03-22 19:50:33 +0100

    actu 1 22-03

--- Practica2/vot_mult.c
+++ Practica2/vot_mult.c
@@ -33,33 +33,36 @@ int down_try(sem_t *sem)
 
 void handlerSIGINT(int sig)
 {
-    printf("\n%d == %d SIGINT %ld\n",sig, SIGINT,(long)getpid());
-    got_sigINT = 1;
-  
-  
+  printf("\n%d == %d SIGINT %ld\n", sig, SIGINT, (long)getpid());
+  got_sigINT = 1;
 }
 
 void handlerSIGUSR1(int sig)
 {
 
-    printf("%d == %d SIGUSR1 %ld\n",sig, SIGUSR1,(long)getpid());
+  printf("%d == %d SIGUSR1 %ld\n", sig, SIGUSR1, (long)getpid());
   got_sigUSR1 = 1;
 }
 
 void handlerSIGTERM(int sig)
 {
-  printf("%d == %d SIGTERM %ld \n",sig, SIGTERM, (long)getpid());
+  printf("%d == %d SIGTERM %ld \n", sig, SIGTERM, (long)getpid());
   got_sigTERM = 1;
+}
+
+void quitar(int sig){
+  printf("%d == %d SIGINT %ld \n", sig, SIGINT, (long)getpid());
 }
 
 void voter();
 void send_signal_sons(int sig, int n_hijos);
 
-void create_sons(int n_procs){
-    /*Generates n_procs and writes their PID in a file*/
-  int pid=0,i;
+/*Generates n_procs and writes their PID in a file*/
+void create_sons(int n_procs)
+{
+  int pid = 0, i;
   FILE *fHijos;
-  
+
   fHijos = fopen(NOMBREFICHERO, "wb");
   if (!fHijos)
   {
@@ -74,8 +77,12 @@ void create_sons(int n_procs){
       /*Function made for the sons*/
       voter();
     }
-    if (pid == -1){
-      send_signal_sons(SIGTERM,i-1);
+    if (pid == -1)
+    {
+      for (i--; i >= 0; i--)
+      {
+        send_signal_sons(SIGTERM, i);
+      }
       exit(EXIT_FAILURE);
     }
 
@@ -84,17 +91,18 @@ void create_sons(int n_procs){
   fclose(fHijos);
 }
 
-void send_signal_sons(int sig, int n_hijos){
-    int pid=0,i,ret;
-    FILE *fHijos;
+void send_signal_sons(int sig, int n_hijos)
+{
+  int pid = 0, i, ret;
+  FILE *fHijos;
 
-    fHijos = fopen(NOMBREFICHERO, "rb");
-    if (!fHijos)
-    {
-      fprintf(stderr, "ERROR opening the file %s to write\n", NOMBREFICHERO);
-      exit(EXIT_FAILURE);
-    }
-    
+  fHijos = fopen(NOMBREFICHERO, "rb");
+  if (!fHijos)
+  {
+    fprintf(stderr, "ERROR opening the file %s to write\n", NOMBREFICHERO);
+    exit(EXIT_FAILURE);
+  }
+
   for (i = 0; i < n_hijos; i++)
   {
     fread(&pid, sizeof(pid_t), 1, fHijos);
@@ -122,26 +130,26 @@ int main(int argc, char *argv[])
   n_procs = atoi(argv[1]);
   n_sec = atoi(argv[2]);
 
-
   create_sons(n_procs);
 
   /*Send every son the signal SIGUSR1*/
-  send_signal_sons(SIGUSR1,n_procs);
-  
+  send_signal_sons(SIGUSR1, n_procs);
+
   /*Change the SIGINT handler*/
   actSIGINT.sa_handler = handlerSIGINT;
   sigemptyset(&(actSIGINT.sa_mask));
   actSIGINT.sa_flags = 0;
   if (sigaction(SIGINT, &actSIGINT, NULL) < 0)
   {
-    perror("sigaction");
+    perror("sigaction SIGINT");
     exit(EXIT_FAILURE);
-  } 
+  }
 
-  while (!got_sigINT);
+  while (!got_sigINT)
+    ;
 
   /*Enviar a cada hijo un SIGTERM*/
-  send_signal_sons(SIGTERM,n_procs);
+  send_signal_sons(SIGTERM, n_procs);
 
   /*Waiting fot the voters*/
   for (i = 0; i < n_procs; i++)
@@ -168,6 +176,18 @@ void voter()
   struct sigaction actSIGUSR1;
   sigset_t mask, oldmask;
 
+  /*Quitar*/
+  actSIGTERM.sa_handler = quitar;
+  sigemptyset(&(actSIGTERM.sa_mask));
+  actSIGTERM.sa_flags = 0;
+
+  if (sigaction(SIGINT, &actSIGTERM, NULL) < 0)
+  {
+    perror("sigaction SIGTERM");
+    exit(EXIT_FAILURE);
+  }
+  /*Quitar*/
+ 
   actSIGUSR1.sa_handler = handlerSIGUSR1;
   sigemptyset(&(actSIGUSR1.sa_mask));
   actSIGUSR1.sa_flags = 0;
@@ -178,28 +198,33 @@ void voter()
 
   if (sigaction(SIGTERM, &actSIGTERM, NULL) < 0)
   {
-    perror("sigaction");
+    perror("sigaction SIGTERM");
     exit(EXIT_FAILURE);
   }
-  
+
   if (sigaction(SIGUSR1, &actSIGUSR1, NULL) < 0)
   {
-    perror("sigaction");
+    perror("sigaction SIGUSR1");
     exit(EXIT_FAILURE);
   }
-  printf("Soy el hijo %ld\n", (long) getpid());
+  printf("Soy el hijo %ld\n", (long)getpid());
 
- /*Mask to block signals SIGUSR1*/ 
- sigemptyset (&mask);
- sigaddset (&mask, SIGUSR1);
- sigprocmask (SIG_BLOCK, &mask, &oldmask);
+  /*Mask to block signals SIGUSR1*/
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGUSR1);
+  sigprocmask(SIG_BLOCK, &mask, &oldmask);
   while (!got_sigUSR1)
-    sigsuspend (&oldmask);
-    
-  sigprocmask (SIG_UNBLOCK, &mask, NULL);
+    sigsuspend(&oldmask);
+
+  sigprocmask(SIG_UNBLOCK, &mask, NULL);
   /* Blocking of signals SIGUSR1 and SIGUSR2 in the process. */
- 
-  while (!got_sigTERM);
+
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGTERM);
+  sigprocmask(SIG_BLOCK, &mask, &oldmask);
+  while (!got_sigTERM)
+    sigsuspend(&mask);
+  printf("Sigterm recibido\n");
 
   printf("Hijo con PID=%ld sale por señal\n", (long)getpid());
   exit(0);
